11ba66b4478c7ce93697cc29a252ae0cb92b6520
Connor J Thompson <ct770a@gmail.com>
2025-01-29 22:50:16 +1100

    updated to example 8 (drawing a cube)

--- Source.cpp
+++ Source.cpp
@@ -8,10 +8,54 @@
 #pragma comment(lib, "d3dcompiler.lib")
 
 #include <assert.h>
+#include <stdint.h>
 
-// https://github.com/kevinmoran/BeginnerDirect3D11/blob/master/02.%20Drawing%20a%20Triangle/main.cpp
+#include "3DMaths.h"
 
 static bool global_windowDidResize = false;
+
+// Input
+enum GameAction {
+    GameActionMoveCamFwd,
+    GameActionMoveCamBack,
+    GameActionMoveCamLeft,
+    GameActionMoveCamRight,
+    GameActionTurnCamLeft,
+    GameActionTurnCamRight,
+    GameActionLookUp,
+    GameActionLookDown,
+    GameActionRaiseCam,
+    GameActionLowerCam,
+    GameActionCount
+};
+static bool global_keyIsDown[GameActionCount] = {};
+
+bool win32CreateD3D11RenderTargets(ID3D11Device1* d3d11Device, IDXGISwapChain1* swapChain, ID3D11RenderTargetView** d3d11FrameBufferView, ID3D11DepthStencilView** depthBufferView)
+{
+    ID3D11Texture2D* d3d11FrameBuffer;
+    HRESULT hResult = swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&d3d11FrameBuffer);
+    assert(SUCCEEDED(hResult));
+
+    hResult = d3d11Device->CreateRenderTargetView(d3d11FrameBuffer, 0, d3d11FrameBufferView);
+    assert(SUCCEEDED(hResult));
+
+    D3D11_TEXTURE2D_DESC depthBufferDesc;
+    d3d11FrameBuffer->GetDesc(&depthBufferDesc);
+
+    d3d11FrameBuffer->Release();
+
+    depthBufferDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+    depthBufferDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+
+    ID3D11Texture2D* depthBuffer;
+    d3d11Device->CreateTexture2D(&depthBufferDesc, nullptr, &depthBuffer);
+
+    d3d11Device->CreateDepthStencilView(depthBuffer, nullptr, depthBufferView);
+
+    depthBuffer->Release();
+
+    return true;
+}
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
@@ -19,9 +63,31 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
     switch (msg)
     {
     case WM_KEYDOWN:
+    case WM_KEYUP:
     {
+        bool isDown = (msg == WM_KEYDOWN);
         if (wparam == VK_ESCAPE)
             DestroyWindow(hwnd);
+        else if (wparam == 'W')
+            global_keyIsDown[GameActionMoveCamFwd] = isDown;
+        else if (wparam == 'A')
+            global_keyIsDown[GameActionMoveCamLeft] = isDown;
+        else if (wparam == 'S')
+            global_keyIsDown[GameActionMoveCamBack] = isDown;
+        else if (wparam == 'D')
+            global_keyIsDown[GameActionMoveCamRight] = isDown;
+        else if (wparam == 'E')
+            global_keyIsDown[GameActionRaiseCam] = isDown;
+        else if (wparam == 'Q')
+            global_keyIsDown[GameActionLowerCam] = isDown;
+        else if (wparam == VK_UP)
+            global_keyIsDown[GameActionLookUp] = isDown;
+        else if (wparam == VK_LEFT)
+            global_keyIsDown[GameActionTurnCamLeft] = isDown;
+        else if (wparam == VK_DOWN)
+            global_keyIsDown[GameActionLookDown] = isDown;
+        else if (wparam == VK_RIGHT)
+            global_keyIsDown[GameActionTurnCamRight] = isDown;
         break;
     }
     case WM_DESTROY:
@@ -50,7 +116,7 @@ int main()
         winClass.cbSize = sizeof(WNDCLASSEXW);
         winClass.style = CS_HREDRAW | CS_VREDRAW;
         winClass.lpfnWndProc = &WndProc;
-        winClass.hInstance = GetModuleHandleW(nullptr);
+        winClass.hInstance = GetModuleHandleW(0);
         winClass.hIcon = LoadIconW(0, IDI_APPLICATION);
         winClass.hCursor = LoadCursorW(0, IDC_ARROW);
         winClass.lpszClassName = L"MyWindowClass";
@@ -68,7 +134,7 @@ int main()
 
         hwnd = CreateWindowExW(WS_EX_OVERLAPPEDWINDOW,
             winClass.lpszClassName,
-            L"02. Drawing a Triangle",
+            L"08. Drawing a Cube",
             WS_OVERLAPPEDWINDOW | WS_VISIBLE,
             CW_USEDEFAULT, CW_USEDEFAULT,
             initialWidth,
@@ -89,9 +155,9 @@ int main()
         ID3D11DeviceContext* baseDeviceContext;
         D3D_FEATURE_LEVEL featureLevels[] = { D3D_FEATURE_LEVEL_11_0 };
         UINT creationFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
-//#if defined(DEBUG_BUILD)
-//        creationFlags |= D3D11_CREATE_DEVICE_DEBUG;
-//#endif
+#if defined(DEBUG_BUILD)
+        creationFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
 
         HRESULT hResult = D3D11CreateDevice(0, D3D_DRIVER_TYPE_HARDWARE,
             0, creationFlags,
@@ -113,22 +179,22 @@ int main()
         baseDeviceContext->Release();
     }
 
-//#ifdef DEBUG_BUILD
-//    // Set up debug layer to break on D3D11 errors
-//    ID3D11Debug* d3dDebug = nullptr;
-//    d3d11Device->QueryInterface(__uuidof(ID3D11Debug), (void**)&d3dDebug);
-//    if (d3dDebug)
-//    {
-//        ID3D11InfoQueue* d3dInfoQueue = nullptr;
-//        if (SUCCEEDED(d3dDebug->QueryInterface(__uuidof(ID3D11InfoQueue), (void**)&d3dInfoQueue)))
-//        {
-//            d3dInfoQueue->SetBreakOnSeverity(D3D11_MESSAGE_SEVERITY_CORRUPTION, true);
-//            d3dInfoQueue->SetBreakOnSeverity(D3D11_MESSAGE_SEVERITY_ERROR, true);
-//            d3dInfoQueue->Release();
-//        }
-//        d3dDebug->Release();
-//    }
-//#endif
+#ifdef DEBUG_BUILD
+    // Set up debug layer to break on D3D11 errors
+    ID3D11Debug* d3dDebug = nullptr;
+    d3d11Device->QueryInterface(__uuidof(ID3D11Debug), (void**)&d3dDebug);
+    if (d3dDebug)
+    {
+        ID3D11InfoQueue* d3dInfoQueue = nullptr;
+        if (SUCCEEDED(d3dDebug->QueryInterface(__uuidof(ID3D11InfoQueue), (void**)&d3dInfoQueue)))
+        {
+            d3dInfoQueue->SetBreakOnSeverity(D3D11_MESSAGE_SEVERITY_CORRUPTION, true);
+            d3dInfoQueue->SetBreakOnSeverity(D3D11_MESSAGE_SEVERITY_ERROR, true);
+            d3dInfoQueue->Release();
+        }
+        d3dDebug->Release();
+    }
+#endif
 
     // Create Swap Chain
     IDXGISwapChain1* d3d11SwapChain;
@@ -175,24 +241,23 @@ int main()
         dxgiFactory->Release();
     }
 
-    // Create Framebuffer Render Target
+    // Create Render Target and Depth Buffer
     ID3D11RenderTargetView* d3d11FrameBufferView;
-    {
-        ID3D11Texture2D* d3d11FrameBuffer;
-        HRESULT hResult = d3d11SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&d3d11FrameBuffer);
-        assert(SUCCEEDED(hResult));
+    ID3D11DepthStencilView* depthBufferView;
+    win32CreateD3D11RenderTargets(d3d11Device, d3d11SwapChain, &d3d11FrameBufferView, &depthBufferView);
 
-        hResult = d3d11Device->CreateRenderTargetView(d3d11FrameBuffer, 0, &d3d11FrameBufferView);
-        assert(SUCCEEDED(hResult));
-        d3d11FrameBuffer->Release();
-    }
+    UINT shaderCompileFlags = 0;
+    // Compiling with this flag allows debugging shaders with Visual Studio
+#if defined(DEBUG_BUILD)
+    shaderCompileFlags |= D3DCOMPILE_DEBUG;
+#endif
 
     // Create Vertex Shader
     ID3DBlob* vsBlob;
     ID3D11VertexShader* vertexShader;
     {
         ID3DBlob* shaderCompileErrorsBlob;
-        HRESULT hResult = D3DCompileFromFile(L"shaders.hlsl", nullptr, nullptr, "vs_main", "vs_5_0", 0, 0, &vsBlob, &shaderCompileErrorsBlob);
+        HRESULT hResult = D3DCompileFromFile(L"shaders.hlsl", nullptr, nullptr, "vs_main", "vs_5_0", shaderCompileFlags, 0, &vsBlob, &shaderCompileErrorsBlob);
         if (FAILED(hResult))
         {
             const char* errorString = NULL;
@@ -215,7 +280,7 @@ int main()
     {
         ID3DBlob* psBlob;
         ID3DBlob* shaderCompileErrorsBlob;
-        HRESULT hResult = D3DCompileFromFile(L"shaders.hlsl", nullptr, nullptr, "ps_main", "ps_5_0", 0, 0, &psBlob, &shaderCompileErrorsBlob);
+        HRESULT hResult = D3DCompileFromFile(L"shaders.hlsl", nullptr, nullptr, "ps_main", "ps_5_0", shaderCompileFlags, 0, &psBlob, &shaderCompileErrorsBlob);
         if (FAILED(hResult))
         {
             const char* errorString = NULL;
@@ -239,8 +304,7 @@ int main()
     {
         D3D11_INPUT_ELEMENT_DESC inputElementDesc[] =
         {
-            { "POS", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-            { "COL", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+            { "POS", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 }
         };
 
         HRESULT hResult = d3d11Device->CreateInputLayout(inputElementDesc, ARRAYSIZE(inputElementDesc), vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), &inputLayout);
@@ -248,20 +312,43 @@ int main()
         vsBlob->Release();
     }
 
-    // Create Vertex Buffer
+    // Create Vertex and Index Buffer
     ID3D11Buffer* vertexBuffer;
-    UINT numVerts;
+    ID3D11Buffer* indexBuffer;
+    // UINT numVerts;
+    UINT numIndices;
     UINT stride;
     UINT offset;
     {
-        float vertexData[] = { // x, y, r, g, b, a
-            0.0f,  0.5f, 0.f, 1.f, 0.f, 1.f,
-            0.5f, -0.5f, 1.f, 0.f, 0.f, 1.f,
-            -0.5f, -0.5f, 0.f, 0.f, 1.f, 1.f
+        float vertexData[] = { // x, y, z
+            -0.5f,-0.5f, -0.5f,
+            -0.5f,-0.5f,  0.5f,
+            -0.5f, 0.5f, -0.5f,
+            -0.5f, 0.5f,  0.5f,
+            0.5f,-0.5f, -0.5f,
+            0.5f,-0.5f,  0.5f,
+            0.5f, 0.5f, -0.5f,
+            0.5f, 0.5f,  0.5f
         };
-        stride = 6 * sizeof(float);
-        numVerts = sizeof(vertexData) / stride;
+
+        uint16_t indices[] = {
+            0, 6, 4,
+            0, 2, 6,
+            0, 3, 2,
+            0, 1, 3,
+            2, 7, 6,
+            2, 3, 7,
+            4, 6, 7,
+            4, 7, 5,
+            0, 4, 5,
+            0, 5, 1,
+            1, 5, 7,
+            1, 7, 3
+        };
+        stride = 3 * sizeof(float);
+        // numVerts = sizeof(vertexData) / stride;
         offset = 0;
+        numIndices = sizeof(indices) / sizeof(indices[0]);
 
         D3D11_BUFFER_DESC vertexBufferDesc = {};
         vertexBufferDesc.ByteWidth = sizeof(vertexData);
@@ -272,12 +359,95 @@ int main()
 
         HRESULT hResult = d3d11Device->CreateBuffer(&vertexBufferDesc, &vertexSubresourceData, &vertexBuffer);
         assert(SUCCEEDED(hResult));
+
+        D3D11_BUFFER_DESC indexBufferDesc = {};
+        indexBufferDesc.ByteWidth = sizeof(indices);
+        indexBufferDesc.Usage = D3D11_USAGE_IMMUTABLE;
+        indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+
+        D3D11_SUBRESOURCE_DATA indexSubresourceData = { indices };
+
+        hResult = d3d11Device->CreateBuffer(&indexBufferDesc, &indexSubresourceData, &indexBuffer);
+        assert(SUCCEEDED(hResult));
     }
+
+    // Create Constant Buffer
+    struct Constants
+    {
+        float4x4 modelViewProj;
+    };
+
+    ID3D11Buffer* constantBuffer;
+    {
+        D3D11_BUFFER_DESC constantBufferDesc = {};
+        // ByteWidth must be a multiple of 16, per the docs
+        constantBufferDesc.ByteWidth = sizeof(Constants) + 0xf & 0xfffffff0;
+        constantBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+        constantBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+        constantBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+
+        HRESULT hResult = d3d11Device->CreateBuffer(&constantBufferDesc, nullptr, &constantBuffer);
+        assert(SUCCEEDED(hResult));
+    }
+
+    ID3D11RasterizerState* rasterizerState;
+    {
+        D3D11_RASTERIZER_DESC rasterizerDesc = {};
+        rasterizerDesc.FillMode = D3D11_FILL_SOLID;
+        rasterizerDesc.CullMode = D3D11_CULL_BACK;
+        rasterizerDesc.FrontCounterClockwise = TRUE;
+
+        d3d11Device->CreateRasterizerState(&rasterizerDesc, &rasterizerState);
+    }
+
+    ID3D11DepthStencilState* depthStencilState;
+    {
+        D3D11_DEPTH_STENCIL_DESC depthStencilDesc = {};
+        depthStencilDesc.DepthEnable = TRUE;
+        depthStencilDesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+        depthStencilDesc.DepthFunc = D3D11_COMPARISON_LESS;
+
+        d3d11Device->CreateDepthStencilState(&depthStencilDesc, &depthStencilState);
+    }
+
+    // Camera
+    float3 cameraPos = { 0, 0, 2 };
+    float3 cameraFwd = { 0, 0, -1 };
+    float cameraPitch = 0.f;
+    float cameraYaw = 0.f;
+
+    float4x4 perspectiveMat = {};
+    global_windowDidResize = true; // To force initial perspectiveMat calculation
+
+    // Timing
+    LONGLONG startPerfCount = 0;
+    LONGLONG perfCounterFrequency = 0;
+    {
+        LARGE_INTEGER perfCount;
+        QueryPerformanceCounter(&perfCount);
+        startPerfCount = perfCount.QuadPart;
+        LARGE_INTEGER perfFreq;
+        QueryPerformanceFrequency(&perfFreq);
+        perfCounterFrequency = perfFreq.QuadPart;
+    }
+    double currentTimeInSeconds = 0.0;
 
     // Main Loop
     bool isRunning = true;
     while (isRunning)
     {
+        float dt;
+        {
+            double previousTimeInSeconds = currentTimeInSeconds;
+            LARGE_INTEGER perfCount;
+            QueryPerformanceCounter(&perfCount);
+
+            currentTimeInSeconds = (double)(perfCount.QuadPart - startPerfCount) / (double)perfCounterFrequency;
+            dt = (float)(currentTimeInSeconds - previousTimeInSeconds);
+            if (dt > (1.f / 60.f))
+                dt = (1.f / 60.f);
+        }
+
         MSG msg = {};
         while (PeekMessageW(&msg, 0, 0, 0, PM_REMOVE))
         {
@@ -287,35 +457,112 @@ int main()
             DispatchMessageW(&msg);
         }
 
+        // Get window dimensions
+        int windowWidth, windowHeight;
+        float windowAspectRatio;
+        {
+            RECT clientRect;
+            GetClientRect(hwnd, &clientRect);
+            windowWidth = clientRect.right - clientRect.left;
+            windowHeight = clientRect.bottom - clientRect.top;
+            windowAspectRatio = (float)windowWidth / (float)windowHeight;
+        }
+
         if (global_windowDidResize)
         {
             d3d11DeviceContext->OMSetRenderTargets(0, 0, 0);
             d3d11FrameBufferView->Release();
+            depthBufferView->Release();
 
             HRESULT res = d3d11SwapChain->ResizeBuffers(0, 0, 0, DXGI_FORMAT_UNKNOWN, 0);
             assert(SUCCEEDED(res));
 
-            ID3D11Texture2D* d3d11FrameBuffer;
-            res = d3d11SwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&d3d11FrameBuffer);
-            assert(SUCCEEDED(res));
-
-            res = d3d11Device->CreateRenderTargetView(d3d11FrameBuffer, NULL,
-                &d3d11FrameBufferView);
-            assert(SUCCEEDED(res));
-            d3d11FrameBuffer->Release();
+            win32CreateD3D11RenderTargets(d3d11Device, d3d11SwapChain, &d3d11FrameBufferView, &depthBufferView);
+            perspectiveMat = makePerspectiveMat(windowAspectRatio, degreesToRadians(84), 0.1f, 1000.f);
 
             global_windowDidResize = false;
         }
 
+        // Update camera
+        {
+            float3 camFwdXZ = normalise({ cameraFwd.x, 0, cameraFwd.z });
+            float3 cameraRightXZ = cross(camFwdXZ, { 0, 1, 0 });
+
+            const float CAM_MOVE_SPEED = 5.f; // in metres per second
+            const float CAM_MOVE_AMOUNT = CAM_MOVE_SPEED * dt;
+            if (global_keyIsDown[GameActionMoveCamFwd])
+                cameraPos += camFwdXZ * CAM_MOVE_AMOUNT;
+            if (global_keyIsDown[GameActionMoveCamBack])
+                cameraPos -= camFwdXZ * CAM_MOVE_AMOUNT;
+            if (global_keyIsDown[GameActionMoveCamLeft])
+                cameraPos -= cameraRightXZ * CAM_MOVE_AMOUNT;
+            if (global_keyIsDown[GameActionMoveCamRight])
+                cameraPos += cameraRightXZ * CAM_MOVE_AMOUNT;
+            if (global_keyIsDown[GameActionRaiseCam])
+                cameraPos.y += CAM_MOVE_AMOUNT;
+            if (global_keyIsDown[GameActionLowerCam])
+                cameraPos.y -= CAM_MOVE_AMOUNT;
+
+            const float CAM_TURN_SPEED = M_PI; // in radians per second
+            const float CAM_TURN_AMOUNT = CAM_TURN_SPEED * dt;
+            if (global_keyIsDown[GameActionTurnCamLeft])
+                cameraYaw += CAM_TURN_AMOUNT;
+            if (global_keyIsDown[GameActionTurnCamRight])
+                cameraYaw -= CAM_TURN_AMOUNT;
+            if (global_keyIsDown[GameActionLookUp])
+                cameraPitch += CAM_TURN_AMOUNT;
+            if (global_keyIsDown[GameActionLookDown])
+                cameraPitch -= CAM_TURN_AMOUNT;
+
+            // Wrap yaw to avoid floating-point errors if we turn too far
+            while (cameraYaw >= 2 * M_PI)
+                cameraYaw -= 2 * M_PI;
+            while (cameraYaw <= -2 * M_PI)
+                cameraYaw += 2 * M_PI;
+
+            // Clamp pitch to stop camera flipping upside down
+            if (cameraPitch > degreesToRadians(85))
+                cameraPitch = degreesToRadians(85);
+            if (cameraPitch < -degreesToRadians(85))
+                cameraPitch = -degreesToRadians(85);
+        }
+
+        // Calculate view matrix from camera data
+        // 
+        // float4x4 viewMat = inverse(rotateXMat(cameraPitch) * rotateYMat(cameraYaw) * translationMat(cameraPos));
+        // NOTE: We can simplify this calculation to avoid inverse()!
+        // Applying the rule inverse(A*B) = inverse(B) * inverse(A) gives:
+        // float4x4 viewMat = inverse(translationMat(cameraPos)) * inverse(rotateYMat(cameraYaw)) * inverse(rotateXMat(cameraPitch));
+        // The inverse of a rotation/translation is a negated rotation/translation:
+        float4x4 viewMat = translationMat(-cameraPos) * rotateYMat(-cameraYaw) * rotateXMat(-cameraPitch);
+        // Update the forward vector we use for camera movement:
+        cameraFwd = { -viewMat.m[2][0], -viewMat.m[2][1], -viewMat.m[2][2] };
+
+        // Spin the cube
+        float4x4 modelMat = rotateXMat(-0.2f * (float)(M_PI * currentTimeInSeconds)) * rotateYMat(0.1f * (float)(M_PI * currentTimeInSeconds));
+
+        // Calculate model-view-projection matrix to send to shader
+        float4x4 modelViewProj = modelMat * viewMat * perspectiveMat;
+
+        // Update constant buffer
+        D3D11_MAPPED_SUBRESOURCE mappedSubresource;
+        d3d11DeviceContext->Map(constantBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedSubresource);
+        Constants* constants = (Constants*)(mappedSubresource.pData);
+        constants->modelViewProj = modelViewProj;
+        d3d11DeviceContext->Unmap(constantBuffer, 0);
+
         FLOAT backgroundColor[4] = { 0.1f, 0.2f, 0.6f, 1.0f };
         d3d11DeviceContext->ClearRenderTargetView(d3d11FrameBufferView, backgroundColor);
 
-        RECT winRect;
-        GetClientRect(hwnd, &winRect);
-        D3D11_VIEWPORT viewport = { 0.0f, 0.0f, (FLOAT)(winRect.right - winRect.left), (FLOAT)(winRect.bottom - winRect.top), 0.0f, 1.0f };
+        d3d11DeviceContext->ClearDepthStencilView(depthBufferView, D3D11_CLEAR_DEPTH, 1.0f, 0);
+
+        D3D11_VIEWPORT viewport = { 0.0f, 0.0f, (FLOAT)windowWidth, (FLOAT)windowHeight, 0.0f, 1.0f };
         d3d11DeviceContext->RSSetViewports(1, &viewport);
 
-        d3d11DeviceContext->OMSetRenderTargets(1, &d3d11FrameBufferView, nullptr);
+        d3d11DeviceContext->RSSetState(rasterizerState);
+        d3d11DeviceContext->OMSetDepthStencilState(depthStencilState, 0);
+
+        d3d11DeviceContext->OMSetRenderTargets(1, &d3d11FrameBufferView, depthBufferView);
 
         d3d11DeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
         d3d11DeviceContext->IASetInputLayout(inputLayout);
@@ -323,9 +570,12 @@ int main()
         d3d11DeviceContext->VSSetShader(vertexShader, nullptr, 0);
         d3d11DeviceContext->PSSetShader(pixelShader, nullptr, 0);
 
-        d3d11DeviceContext->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
+        d3d11DeviceContext->VSSetConstantBuffers(0, 1, &constantBuffer);
 
-        d3d11DeviceContext->Draw(numVerts, 0);
+        d3d11DeviceContext->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
+        d3d11DeviceContext->IASetIndexBuffer(indexBuffer, DXGI_FORMAT_R16_UINT, 0);
+
+        d3d11DeviceContext->DrawIndexed(numIndices, 0, 0);
 
         d3d11SwapChain->Present(1, 0);
     }
